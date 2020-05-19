b83e5b8ae110164bc62d1ab11da3867724197920
Ramon Donadeu <ramondonadeucaballero@gmail.com>
2020-05-19 21:00:46 +0200

    Ordena be

--- code.py
+++ code.py
@@ -4,14 +4,14 @@ import pickle
 import time
 import datetime
 
-N_SLAVES = 3
-nom_cos = 'sdurv'
+N_SLAVES = 20
+nom_cos = 'ramonsd'
 fitxer = 'p_write_'
 TIME = 1
 
 
 def master(id, x, ibm_cos):
-    #Esperem 1 segon a que almenys algun slave hagi creat el seu fitxer
+    # Esperem 1 segon a que almenys algun slave hagi creat el seu fitxer
     time.sleep(1)
     objectes = True
     i = 0
@@ -19,16 +19,14 @@ def master(id, x, ibm_cos):
     fitxer = ""
     write_permission_list = []
     llista = []
-    continguts = []
-    diccionari = {}
-   
-   #Guardem al moment de creació del result
+
+   # Guardem al moment de creació del result
     ibm_cos.put_object(Bucket=nom_cos, Key='result.txt',
                        Body=pickle.dumps(data, pickle.HIGHEST_PROTOCOL))
     json = ibm_cos.list_objects(Bucket=nom_cos, Prefix='result')
     # Agafem el temps i eliminem la zona horaria.
     json_time = json['Contents'][0]['LastModified'].replace(tzinfo=None)
-    temps_anterior=json_time
+    temps_anterior = json_time
 
     # 1. monitor COS bucket each X seconds
     while objectes:
@@ -36,30 +34,24 @@ def master(id, x, ibm_cos):
         # 2. List all "p_write_{id}" files
         try:
             llista = ibm_cos.list_objects(Bucket=nom_cos, Prefix='p_write')
-            continguts = llista['Contents']  # Agafem els valors de "Contents"
             i = 0
-            while (i < len(continguts)):  # Llegim els Contents de tots els fitxer que hem obtingut
-                # Guardem la ultima data de modificacio a Dates
-                dates = []
-                dates.append(continguts[i]['LastModified'])
-                # Creem un diccionari amb les Dates : Nom del fitxer
-                diccionari[continguts[i]['LastModified']] = continguts[i]['Key']
-                i = i+1
-
+            dates= []
+            for dic in llista['Contents']:
+                dates.append([dic['Key'], dic ['LastModified']])
+                
             # 3. Order objects by time of creation
-            dates.sort()  # Ordenem el vector de les dates
-
+            dates.sort(key=lambda x: x[1])
             # 4. Pop first object of the list "p_write_{id}"
-            pop = dates.pop(0)
+            pop = dates[0][0]
             # Obtenim el nom del primer fitxer ordenat.
-            fitxer = str(diccionari[pop])
+            fitxer = str(pop)
 
             # 5. Write empty "write_{id}" object into COS
             # Separem el fitxer del cos i el separem per "_"
             id = fitxer.split("_", 1)
-            nom_fitxer = id[1] # Ens quedem amb la segona part "write_{id}"
+            nom_fitxer = id[1]  # Ens quedem amb la segona part "write_{id}"
             ibm_cos.put_object(Bucket=nom_cos, Key=nom_fitxer,
-                            Body=pickle.dumps(data, pickle.HIGHEST_PROTOCOL))
+                               Body=pickle.dumps(data, pickle.HIGHEST_PROTOCOL))
 
             # 6. Delete from COS "p_write_{id}", save {id} in write_permission_list
             ibm_cos.delete_object(Bucket=nom_cos, Key=fitxer)
@@ -78,15 +70,14 @@ def master(id, x, ibm_cos):
                 json_time = json['Contents'][0]['LastModified'].replace(
                     tzinfo=None)
                 if (temps_anterior < json_time):
-                    updated=True
+                    updated = True
                     temps_anterior = json_time
-            
 
             # 8. Delete from COS “write_{id}”
             ibm_cos.delete_object(Bucket=nom_cos, Key=nom_fitxer)
         except:
             objectes = False
-       
+
         time.sleep(TIME)
         # 9. Back to step 1 until no "p_write_{id}" objects in the bucket
     return write_permission_list
@@ -96,12 +87,12 @@ def slave(id, x, ibm_cos):
 
     data = ''
     # 1. Write empty "p_write_{id}" object into COS
-    nom_fitxer =  'p_write_{' + str(id) + '}'
+    nom_fitxer = 'p_write_{' + str(id) + '}'
     ibm_cos.put_object(Bucket=nom_cos, Key=nom_fitxer,
                        Body=pickle.dumps(data, pickle.HIGHEST_PROTOCOL))
 
     # 2. Monitor COS bucket each X seconds until it finds a file called "write_{id}"
-    nom_fitxer =  'write_{' + str(id) + '}'
+    nom_fitxer = 'write_{' + str(id) + '}'
     disponible = False
     while (disponible == False):
         try:
@@ -119,8 +110,9 @@ def slave(id, x, ibm_cos):
     resultat.append(id)
     ibm_cos.put_object(Bucket=nom_cos, Key='result.txt',
                        Body=pickle.dumps(resultat, pickle.HIGHEST_PROTOCOL))
-    
+
     # 4. Finish
+
 
 if __name__ == '__main__':
     pw = pywren.ibm_cf_executor()
@@ -133,7 +125,7 @@ if __name__ == '__main__':
 
     # Get result.txt
     cos = COSBackend()
-    results = cos.get_object('sdurv', 'result.txt')
+    results = cos.get_object('ramonsd', 'result.txt')
     results = pickle.loads(results)
     print("El resultat es:")
     print(results)
